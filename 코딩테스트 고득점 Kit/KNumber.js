3e80f0374472a697cc0738bcb73719191f6077bb
BangBang-e <youngjae7133@gmail.com>
2023-07-18 22:08:30 +0900

    2023.07.18

---
+++ 코딩테스트 고득점 Kit/KNumber.js
@@ -0,0 +1,48 @@
+//? [K번째 수]
+//TODO <문제 설명>
+//TODO 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+//TODO 예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+//! 1. array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+//! 2. 1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+//! 3. 2에서 나온 배열의 3번째 숫자는 5입니다.
+//TODO 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때,
+//TODO commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+//! <제한 사항>
+//! array의 길이는 1 이상 100 이하입니다.
+//! array의 각 원소는 1 이상 100 이하입니다.
+//! commands의 길이는 1 이상 50 이하입니다.
+//! commands의 각 원소는 길이가 3입니다.
+
+//* <입출력 예>	
+//*   array => [1, 5, 2, 6, 3, 7, 4]
+//*   commands => [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+//*   return => [5, 6, 3]
+
+//::::
+
+function solution(array, commands) {
+    let answer = [];
+    let newArray = array;
+
+    for (let i = 0; i < commands.length; i++) {
+        let elem = commands[i];
+        let slicedArr = newArray.slice(elem[0] - 1, elem[1]).sort((a, b) => a - b);
+        answer.push(slicedArr[elem[2] - 1]);
+    }
+    return answer;
+}
+console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])); // [5, 6, 3]
+
+//-------------------------------------------------------------------------------------------
+
+//? [다른 풀이]
+
+// function solution2(array, commands) {
+//     return commands.map((command) => {
+//         let [start, end, index] = command;
+//         return array.slice(start - 1, end).sort((a, b) => a - b)[index - 1];
+//     });
+// }
+
+// console.log(solution2([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])); // [5, 6, 3]
